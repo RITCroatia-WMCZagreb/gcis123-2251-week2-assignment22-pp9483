8aac8a10e6aa0e9cb4afdd1f64167ec58f04c3bc
pp9483 <pp9483@g.rit.edu>
2025-09-16 22:18:08 +0200

    working version

--- characters.py
+++ characters.py
@@ -1,7 +1,45 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: pp9483
+@ASSESSME.AUTHOR: Patrik Pavlovic
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+#one = input("Please enter an ASCII character:")
+#two = input("Please enter another ASCII character:")
 
+##onea = ord(one)
+#twoa = ord(two)
+
+
+def add_chars(onea,twoa,one,two):
+    print("The sum of your ASCII characters is:",onea + twoa,"and your ASCII characters were:",one,two)
+
+
+def subtract_chars(onea,twoa,one,two):
+    print("The difference between the ASCII characters is:",onea - twoa,"and your ASCII characters were:",one,two)
+
+
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+
+def main():
+    one = input("Please enter an ASCII character:")
+    two = input("Please enter another ASCII character:")
+    onea = ord(one)
+    twoa = ord(two)
+    add_chars(onea,twoa,one,two)
+    subtract_chars(onea,twoa,one,two)
+
+main()
--- scope.py
+++ scope.py
@@ -1,6 +1,31 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: pp9483
+@ASSESSME.AUTHOR: Patrik
 @ASSESSME.DESCRIPTION: Assignment 2.2
 @ASSESSME.ANALYZE: YES
 '''
+
+STRING_GLOBAL = str
+INT_GLOBAL = int
+FLOAT_GLOBAL = float
+
+
+def print_param(pmeter):
+    print(pmeter)
+
+def print_local():
+    local = "i guess"
+    print(local)
+
+def print_which():
+    FLOAT_GLOBAL = "it's not"
+    print(FLOAT_GLOBAL)
+
+
+def main():
+    print_param()
+    print_local()
+    print_which()
+
+
+main()
